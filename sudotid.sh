f561e922524304be62d5c61383b0d63ff071fda5
Can Yesilyurt <can--yesilyurt@hotmail.com>
2024-10-05 12:33:06 +0300

    Update sudotid.sh

--- sudotid.sh
+++ sudotid.sh
@@ -4,8 +4,6 @@ cp /etc/pam.d/sudo /tmp/sudotmp
 echo "auth sufficient pam_tid.so" > /tmp/sudotmp2
 cat /tmp/sudotmp >> /tmp/sudotmp2
 sudo mv /tmp/sudotmp2 /etc/pam.d/sudo
-rm -r /tmp/sudotmp
-rm -r /tmp/sudotmp2
-
+rm /tmp/sudotmp
 
 exit 0
